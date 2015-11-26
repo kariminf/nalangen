73bb51e9e47bb1d0be11d22f4fac56b40b7bb0d4
Abdelkrime Aries <kariminfo0@gmail.com>
2015-11-26 11:35:40 +0100

    search for synonyms, search for a synset number

--- src/main/dz/aak/nalangen/wordnet/WNRequestor.java
+++ src/main/dz/aak/nalangen/wordnet/WNRequestor.java
@@ -1,7 +1,13 @@
 package dz.aak.nalangen.wordnet;
 
+import java.util.List;
+
 public interface WNRequestor {
 	
 	public String getWord(int synset, String pos);
+	
+	public List<String> getWords(int synset, String pos);
+	
+	public int getSynset(String word, String pos);
 
 }
